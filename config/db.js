41f4def23b19a25c42b0e67654364f40626d73b7
sajibkhan33 <nurnavi929@gmail.com>
2025-10-03 00:23:30 +0600

    second-commit

---
+++ config/db.js
@@ -0,0 +1,31 @@
+// pss: u84za8gPt1j6PdFF
+// dbName: quickCArtdb
+
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    imageUrl: { type: String, require: true },
+    cartItems: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
